21ed08414cd9889df1289845cabf70773c692f57
TabooYY <144597324+TabooYY@users.noreply.github.com>
2023-10-26 13:36:58 +0300

    Update 18.8.19(HW-03).py

--- 18.8.19(HW-03).py
+++ 18.8.19(HW-03).py
@@ -1,1 +1,21 @@
-# Task-18.8.19-HW-03-
+amount = 0
+tickets = int(input("Введите количество билетов: "))
+for age in range(tickets):
+    age = int(input("Введите возраст посетителя: "))
+    if age < 18:
+        amount += 0
+    elif age >= 18 and age <= 25:
+        amount += 990
+    elif age > 25:
+        amount += 1390
+if amount == 0:
+    print("Дети проходят бесплатно!")
+else:
+    print("Кол-во билетов:", tickets, "шт." )
+if tickets > 3 :
+    discount = amount / 100 * 10
+    print("Скидка составляет:", "%.2f" % discount, "руб.")
+    print("К оплате с учетом скидки:", "%.2f" % (amount -discount), "руб.")
+if tickets < 4 :
+    Nodiscount =amount
+    print("К оплате:", "%.2f" % Nodiscount, "руб.")
